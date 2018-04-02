6b46e8f3d9cbd48e272479c133247846fd8a7b5e
Arucis <michaelborisov9@gmail.com>
2018-04-02 23:17:12 +0200

    clean up

--- BestBot/src/main/java/Main/Methods.java
+++ BestBot/src/main/java/Main/Methods.java
@@ -1,21 +1,20 @@
 package Main;
 
-import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.MessageChannel;
 
 public class Methods {
-	
-	//generate random number between 0 und max
+
+	// generate random number between 0 und max
 	public static int randomNumber(int max) {
 		int randomNumber = (int) (Math.random() * max);
 
 		return randomNumber;
 	}
-	
-	//prints a coin flip
+
+	// prints a coin flip
 	protected static void coinFlip(MessageChannel objCh) {
 		int randomNumber = (int) (Math.random() * 2);
-		
+
 		if (randomNumber == 0)
 			objCh.sendMessage("Heads").queue();
 		else
