c5a79dba447878438e1acfbdcd78e44370360257
Arucis <michaelborisov9@gmail.com>
2018-04-06 12:27:02 +0200

    deleted the jda thingy

--- BestBot/src/main/java/Main/Bot.java
+++ BestBot/src/main/java/Main/Bot.java
@@ -4,7 +4,6 @@ import com.jagrosh.jdautilities.command.CommandClientBuilder;
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
 
 import net.dv8tion.jda.core.AccountType;
-import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
@@ -17,9 +16,7 @@ public class Bot extends ListenerAdapter {
 		client.setOwnerId(ownerId);
 		client.addCommands(new RandomNumber(), new Ping(), new Flip());
 
-		JDA jda = new JDABuilder(AccountType.BOT).setToken(Settings.token).addEventListener(waiter)
+		new JDABuilder(AccountType.BOT).setToken(Settings.token).addEventListener(waiter)
 				.addEventListener(client.build()).buildAsync();
-
-		jda.addEventListener(new Bot());
 	}
 }
