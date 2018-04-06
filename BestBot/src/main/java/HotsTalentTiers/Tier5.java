773df24b569d854a313392d273a0d963dd94cf35
Tunc <tunc-erdogan18@hotmail.de>
2018-04-06 14:24:02 +0200

    Hots Hero Struktur und einige Talente hinzugefügt

---
+++ BestBot/src/main/java/HotsTalentTiers/Tier5.java
@@ -0,0 +1,12 @@
+package HotsTalentTiers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tier5 {
+List<String> t5 = new ArrayList<>();
+	
+	public Tier5(List<String> t5) {
+		this.t5 = t5;
+	}
+}
---
+++ BestBot/src/main/java/HotsTalentTiers/Tier7.java
@@ -0,0 +1,12 @@
+package HotsTalentTiers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tier7 {
+	List<String> t7 = new ArrayList<>();
+	
+	public Tier7(List<String> t7) {
+		this.t7 = t7;
+	}
+}
